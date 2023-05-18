dd65549cb1dbf660e569da82e73f566b13167599
bkmashiro <dylan_233@foxmail.com>
2023-05-18 21:27:39 +0800

    improved security, added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,12 +9,15 @@ async function bootstrap() {
   app.enableCors();
 
   const config = new DocumentBuilder()
-    .setTitle('管理后台')
+    .setTitle('Xlxu系统管理后台')
     .setDescription('管理后台接口文档')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/apidoc', app, document); 
+
+
+  app.use(helmet());
 
   await app.listen(3000);
 }
