feb9a1234190736efbe03db551dde72009238f58
bkmashiro <dylan_233@foxmail.com>
2023-10-21 16:23:32 +0800

    clean

--- src/modules/chatgroup/chatgroup.controller.ts
+++ src/modules/chatgroup/chatgroup.controller.ts
@@ -3,8 +3,7 @@ import { ChatgroupService } from './chatgroup.service';
 import { CreateChatgroupDto } from './dto/create-chatgroup.dto';
 import { UpdateChatgroupDto } from './dto/update-chatgroup.dto';
 import { JoinChatGroupDto } from './dto/join-chatgroup.dto';
-import { AdminRole, Roles, UserRole } from '../auth/roles/roles.decorators';
-import { ROLES } from '../auth/roles/roles.constants';
+import { AdminRole, UserRole } from '../auth/roles/roles.decorators';
 import { IsAdmin, User } from '../common/decorator/decorators';
 import { User as UserType } from '../user/entities/user.entity';
 import { AddAdminDto } from './dto/add-admin-chatgroup.dto';
@@ -71,5 +70,4 @@ export class ChatgroupController {
   getGroupMembers(@Param('id') id: string) {
     return this.chatgroupService.getMembers(+id);
   }
-
 }
